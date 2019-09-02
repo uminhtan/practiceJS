465dca175c3982dba34ea49fc750b5270e25afae
minhTan <minhtu471100@gmail.com>
2019-09-02 18:41:39 +0700

    about function

---
+++ function.js
@@ -0,0 +1,11 @@
+function calculateTriangleleSquare (a, h) {
+    return a * h / 2;
+} // store in RAM
+calculateTriangleleSquare (10, 5);
+console.log(calculateTriangleleSquare (10, 5));
+// round square
+// s = r * r * 3.14
+function roundSquare(r) {
+    return r * r * 3.14;
+}
+console.log(roundSquare(10));
