9a979263df454a2af687025bcea2076fa7dc5dcc
minhTan <minhtu471100@gmail.com>
2019-09-04 17:53:24 +0700

    about compare but more specific

--- compare.js
+++ compare.js
@@ -4,4 +4,13 @@ var a = {
 var b = {
     foo: 'bar'
 };
-console.log(a === b);
+console.log(a === b); 
+// kết quả là false bởi vì hai object không tham chiếu đến cùng một ô nhớ 
+
+
+
+var a = '1000';
+var b = '200';
+
+console.log(a > b); 
+// kết quả là false vì nội dung hai chuỗi khác nhau 
